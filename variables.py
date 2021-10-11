f48fdd15700aa7cf78b78452f441f9048874d70f
Hivin <49328442+SilverCrocus@users.noreply.github.com>
2021-10-11 20:18:24 +1300

    deleting variables file

--- variables.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.environ['reddit_client_id'] = 'oKLcCQ6OjNHv9CXDIrOM0Q'
-os.environ['reddit_client_secret'] = 'dMzfMPHJKpgrr3eH55FyofI4l80TWA'
-os.environ["reddit_password"] = "batfatman123"
-os.environ["reddit_username"] = "iownredditlol"
